4702ddab7441056fda77fd6d919d60dba4d93e2f
tlherr <thom1810@gmail.com>
2016-12-13 08:14:10 -0500

    Initial Commit

---
+++ FinalExam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalExam.Startup))]
+namespace FinalExam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
